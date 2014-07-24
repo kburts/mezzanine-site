518df6d1ef9451dbf082c3e8063363612755a915
kburts <kevin.burton2@gmail.com>
2014-07-23 19:10:29 -0700

    Added portfolio-detail view/url

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from .models import Item
 # Create your views here.
 
 class PortfolioListView(ListView):
 
+    model = Item
+
+class PortfolioDetailView(DetailView):
     model = Item
